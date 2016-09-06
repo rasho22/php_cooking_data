11c9da9e1e869c9de8f3b2991d667ac389309c61
rasho22 <ab_rasho@yahoo.fr>
2016-09-06 17:02:20 +0200

    avancement

--- Solution.php
+++ Solution.php
@@ -43,13 +43,75 @@ echo count($lettreQ);
 echo "<br>";
 ?>
 
+<pre>
+
 
 <?php
+
+//FILMS
+
 	$string = file_get_contents("films.json", FILE_USE_INCLUDE_PATH);
 	$brut = json_decode($string, true);
 	$top = $brut["feed"]["entry"];
-	$array= count($top);
-	print_r($array);
+	$arraylenght = count($top);
+	print_r($top[0]['im:name']['label']);
 
-?>
+	print_r($top);
+	?>
+</pre>
 
+
+
+<pre>
+
+		<h3>
+			<?php
+			//les 10top films
+			echo '<ol>';
+			for ($i=0; $i < 10; $i++) { 
+				
+				echo '<li>'.$top[$i]['im:name']['label']. '</li>';
+				
+			}
+			echo'</ol>';
+
+			?>
+		</h3>
+		
+</pre>
+
+<?php
+
+//trouver la position de Gravity
+
+	for ($i=0; $i < $arraylenght; $i++) { 
+	if ($top[$i]['im:name']['label'] == "Gravity") {
+		echo 'Gravity se trouve à la position: '.$i;
+	}
+}
+
+echo "<br>";
+//le réalisteur de film The LEGO Movie
+
+	for ($director=0; $director < $arraylenght; $director++) { 
+		if ($top[$director]['im:name']['label'] == "The LEGO Movie") {
+			echo $top[$director]['im:artist']['label'];
+		}
+	}
+
+echo "<br>";
+
+//les films sont soties avant 2000
+$dateSortie=0;
+for ($i=0; $i < ; $i++) { 
+	# code...
+}
+
+/*
+$date=$top[0]['im:releaseDate']['label']<'2000';
+for ($i=1; $i < $list; $i++) { 
+	if ($date<$top[$i]['im:releaseDate']['label']) {
+		# code...
+	}
+}*/
+	?>	
